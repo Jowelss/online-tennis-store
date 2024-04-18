18c62d1895e8e42d88c5e64e2c0270c5e74e07d5
Jowelss <devejowels.gitl.loper@gmail.com>
2024-04-17 23:00:33 -0400

    nothing

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const product = document.querySelector('.main-product__content');
+
+// Agregar a este array cada producto cuando el usuario de click en 'add to cart'
+// Mi idea ya esta mas o menos clara
+const cart = [{ item: { name: '', color: '', size: '' } }];
