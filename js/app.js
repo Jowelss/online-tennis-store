a8aa839f202941d69afaf1540ddd968e05ee4d7f
Jowelss <devejowels.gitl.loper@gmail.com>
2024-04-23 19:10:05 -0400

    delete

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const buttonNext = document.getElementById('button_next');
-const template = document.getElementById('template');
-const fragment = document.createDocumentFragment();
-
-buttonNext.addEventListener('click', () => {
-  const copyTemplate = document.importNode(template.content, true);
-  const elements = copyTemplate.querySelectorAll('.product');
-  console.log(elements);
-});
