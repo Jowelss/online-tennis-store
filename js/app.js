4deaea1fba3210fa984936b25e9c2aa56c382ff6
Jowelss <devejowels.gitl.loper@gmail.com>
2024-04-26 20:02:55 -0400

    continued later

--- js/app.js
+++ js/app.js
@@ -2,16 +2,9 @@ const colors = document.querySelectorAll('.color-list__link');
 
 const cart = [];
 
-const addCart = (color) => {
-  if (cart < 2) {
-    cart.push(color);
-  } else {
-    console.log('coso');
-  }
-};
-
 colors.forEach((element) => {
   element.addEventListener('click', (e) => {
-    addCart(e.target.id);
+    cart.push(e.target.id);
+    console.log(cart);
   });
 });
