ff371254b8d84f2d096bf6b1bb3c5760a2533edd
Jowelss <devejowels.gitl.loper@gmail.com>
2024-05-02 18:52:28 -0400

    added new code and it seems to work

--- js/app.js
+++ js/app.js
@@ -1,12 +1,34 @@
 const list = document.querySelectorAll('.list__select');
+const nameProduct = document.querySelector('.product-title').textContent;
 const buttonAdd = document.getElementById('buys-add');
 
-function extraerDatos() {
-  for (let i = 0; i < list.length; i++) {
-    const element = list[i];
+const styleProduct = [];
 
-    console.log(element);
+function addCart() {
+  const cart = [];
+
+  const coso = {
+    nombre: nameProduct,
+    color: styleProduct[0],
+    size: styleProduct[1],
+  };
+
+  cart.push(coso);
+  console.log(cart);
+}
+
+function extraerDatos(evento) {
+  if (styleProduct.length < 2) {
+    styleProduct.includes(evento)
+      ? console.log('El elemento ya existe en el carrito')
+      : styleProduct.push(evento);
+  } else {
+    console.log('Máximo de elecciones alcanzadas');
   }
 }
 
-extraerDatos();
+buttonAdd.addEventListener('click', addCart);
+
+list.forEach((item) => {
+  item.addEventListener('click', (e) => extraerDatos(e.target.value));
+});
