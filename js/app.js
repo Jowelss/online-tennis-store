64d4175ba138b0c01eb3e821d453373443579c92
Jowelss <devejowels.gitl.loper@gmail.com>
2024-05-03 20:48:55 -0400

    reduced code, desired method

--- js/app.js
+++ js/app.js
@@ -1,50 +1,36 @@
 const colorList = document.querySelectorAll('.color-list__select');
+const image = document.querySelector('.picture-image');
 const sizeList = document.querySelectorAll('.size-list__select');
 const nameProduct = document.querySelector('.product-title').textContent;
 const buttonAdd = document.getElementById('buys-add');
 
-const colorProduct = [];
-const sizeProduct = [];
+const cart = [];
+
+const data = {};
 
 function addCart() {
-  const cart = [];
-
-  const coso = {
-    nombre: nameProduct,
-    color: colorProduct[0],
-    size: sizeProduct[0],
-  };
-
-  cart.push(coso);
-  console.log(coso);
+  const newDate = { ...data };
+  cart.push(newDate);
+  console.log(cart);
 }
 
-function extraerDatosColor(evento) {
-  if (colorProduct.length < 1) {
-    colorProduct.includes(evento)
-      ? console.log('El elemento ya existe en el carrito')
-      : colorProduct.push(evento);
-  } else {
-    console.log('Máximo de elecciones alcanzadas');
-  }
+function extraerDatosColor(e) {
+  sizeList.forEach((size) => {
+    size.value !== e ? (data['color'] = e) : console.log('No es distinto');
+  });
 }
-
-function extraerDatosSize(evento) {
-  if (sizeProduct.length < 1) {
-    sizeProduct.includes(evento)
-      ? console.log('El elemento ya existe')
-      : sizeProduct.push(evento);
-  } else {
-    console.log('Máximo de elecciones alcanzadas');
-  }
+function extraerDatosSize(e) {
+  colorList.forEach((color) => {
+    color.value !== e ? (data['size'] = e) : console.log('No es distinto');
+  });
 }
 
 buttonAdd.addEventListener('click', addCart);
 
-colorList.forEach((item) => {
-  item.addEventListener('click', (e) => extraerDatosColor(e.target.value));
+colorList.forEach((itemColor) => {
+  itemColor.addEventListener('click', (e) => extraerDatosColor(e.target.value));
 });
 
-sizeList.forEach((item) => {
-  item.addEventListener('click', (e) => extraerDatosSize(e.target.value));
+sizeList.forEach((itemSize) => {
+  itemSize.addEventListener('click', (e) => extraerDatosSize(e.target.value));
 });
