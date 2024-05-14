6a63ff5cdbef8ece7b5e8631009a67c8b24784ec
Jowelss <devejowels.gitl.loper@gmail.com>
2024-05-13 22:24:04 -0400

    add template and his childs for are the event

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ const colorList = document.querySelectorAll('.color-list__select');
 const buttonAdd = document.getElementById('buys-add');
 const input = document.querySelectorAll('input');
 const sizeList = document.querySelectorAll('.size-list__select');
-const buttonTraslateRigth = document.querySelector('.button-next__content');
+const buttonTraslateRigth = document.querySelector('.button-next__content'); // boton para cambiar de porducto
 const main = document.querySelector('.main'); //Para agregar las dos clases para que el evento funcione
+const template = document.getElementById('template');
+const productTitle = document.querySelector('.product-title');
+const productInfo = document.querySelector('.product-info__text');
 
 const cart = [];
 
@@ -41,9 +44,14 @@ sizeList.forEach((itemSize) => {
 });
 
 buttonTraslateRigth.addEventListener('click', () => {
+  const copyTemplate = document.importNode(template.content, true);
+  const adidasTitle = copyTemplate.querySelector('.adidas-title').textContent;
+  const adidasInfo = copyTemplate.querySelector('.adidas-info').textContent;
   if (!main.classList.contains('traslate-left')) {
     main.classList.add('traslate-left');
     setTimeout(() => {
+      productTitle.textContent = adidasTitle;
+      productInfo.textContent = adidasInfo;
       main.classList.remove('traslate-left');
       main.classList.add('traslate-right');
     }, 1000);
